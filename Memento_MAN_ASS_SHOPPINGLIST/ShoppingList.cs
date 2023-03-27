6bf6f2eb93d3228b740001a5c8993753cc0dbdd2
Madsthomsen123 <104567673+Madsthomsen123@users.noreply.github.com>
2023-03-27 13:10:02 +0200

    Classes made

---
+++ Memento_MAN_ASS_SHOPPINGLIST/ShoppingList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memento_MAN_ASS_SHOPPINGLIST
+{
+	public class ShoppingList
+	{
+
+	}
+}
