a12be9a41ce5afcdb2bc77b035f081ccb9b59768
JonasKnudsen <jonasfbknudsen@gmail.com>
2023-03-29 14:20:33 +0200

    more comments

--- Memento_MAN_ASS_SHOPPINGLIST/Caretaker.cs
+++ Memento_MAN_ASS_SHOPPINGLIST/Caretaker.cs
@@ -6,33 +6,6 @@ using System.Threading.Tasks;
 
 namespace Memento_MAN_ASS_SHOPPINGLIST
 {
-	/*public class ShoppingListCaretaker
-	{
-		private readonly Stack<ShoppingListMemento> _mementos = new Stack<ShoppingListMemento>();
-		private readonly IShoppingList _shoppingList;
-
-		public ShoppingListCaretaker(IShoppingList shoppingList)
-		{
-			_shoppingList = shoppingList;
-		}
-
-		public void AddMemento()
-		{
-			_mementos.Push(_shoppingList.Save());
-		}
-
-		public void Undo()
-		{
-			if (_mementos.Count == 0)
-			{
-				Console.WriteLine("Cannot undo: No previous state.");
-				return;
-			}
-
-			var memento = _mementos.Pop();
-			_shoppingList.Restore(memento);
-		}
-	}*/
 	// This class implements the caretaker role in the memento pattern
 	public class ShoppingListCaretaker
 	{
