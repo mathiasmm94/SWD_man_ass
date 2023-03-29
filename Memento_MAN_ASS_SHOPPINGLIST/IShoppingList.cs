7537f0638b5a608fd1a9290583bbe438e38114e6
JonasKnudsen <jonasfbknudsen@gmail.com>
2023-03-29 14:22:44 +0200

    Comments to all

--- Memento_MAN_ASS_SHOPPINGLIST/IShoppingList.cs
+++ Memento_MAN_ASS_SHOPPINGLIST/IShoppingList.cs
@@ -6,57 +6,66 @@ using System.Threading.Tasks;
 
 namespace Memento_MAN_ASS_SHOPPINGLIST
 {
+	// This interface defines the methods for a shopping list
 	public interface IShoppingList
 	{
+		// This method adds an item to the shopping list
 		public void AddItem(string item);
 
+		// This method removes an item from the shopping list
 		public void RemoveItem(string item);
 
+		// This method prints the items in the shopping list
 		public void PrintItems();
 
+		// This method creates and returns a memento object that stores the current state of the shopping list
 		public ShoppingListMemento Save();
 
+		// This method restores the state of the shopping list from a given memento object
 		public void Restore(ShoppingListMemento memento);
 	}
 
+	// This class implements the IShoppingList interface using a list of strings
 	public class ShoppingList : IShoppingList
 	{
+		// This field holds the items in the shopping list
 		private List<string> _items = new List<string>();
 
+		// This method adds an item to the shopping list
 		public void AddItem(string item)
 		{
 			_items.Add(item);
 		}
 
+		// This method removes an item from the shopping list
 		public void RemoveItem(string item)
 		{
 			_items.Remove(item);
 		}
 
+		// This method creates and returns a memento object that stores the current state of the shopping list
 		public ShoppingListMemento Save()
 		{
 			return new ShoppingListMemento(_items);
 		}
 
+		// This method restores the state of the shopping list from a given memento object
 		public void Restore(ShoppingListMemento memento)
 		{
 			_items = memento.GetItems();
 		}
 
+		// This method prints the items in the shopping list
 		public void PrintItems()
 		{
 			Console.WriteLine("Shopping List:");
-            for (int i = 0; i < _items.Count; i++)
-            {
-                Console.WriteLine($"{i} {_items[i]}");
-            }
-			//foreach (var item in _items)
-			//{
-			//	Console.WriteLine(item);
-   //         }
-            Console.WriteLine("_________________");
-            Console.WriteLine("");
-        }
+			for (int i = 0; i < _items.Count; i++)
+			{
+				Console.WriteLine($"{i} {_items[i]}");
+			}
+			Console.WriteLine("_________________");
+			Console.WriteLine("");
+		}
 	}
 
 
